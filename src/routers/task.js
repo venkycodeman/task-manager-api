3236abf5f21c3ae6198f4ad942be59de998319b6
Venkatesh Babu <polineni36@gmail.com>
2020-06-13 23:02:40 +0530

    initial commit

---
+++ src/routers/task.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const router = new express.Router()
+const auth = require('../middleware/auth')
+const Task = require('../models/task')
+
+router.post('/tasks', auth, async (req, res)=>{
+        const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
+    try{
+        await task.save()
+        res.status(201).send(task)
+    }catch(e){
+        
+        res.status(400).send()
+    }
+    
+
+})
+
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt:desc
+
+router.get('/tasks',auth, async (req, res)=>{
+    const match ={}
+    //my confusion got cleared : point is req.query.completed is string value
+    const sort = {}
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+    try{
+        
+        await req.user.populate({
+            path:'tasks',
+            match,
+            options:{
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
+        res.send(req.user.tasks)
+    } catch(e){
+        console.log(e)
+        res.status(500).send()
+    }
+    
+})
+
+router.get('/tasks/:id', auth, async (req, res)=>{
+    const _id = req.params.id
+
+    try{
+        
+        const task = await Task.findOne({_id, owner: req.user._id})
+
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    } catch(e){
+        res.status(500).send()
+    }
+  
+})
+
+router.patch('/tasks/:id',auth ,async (req, res)=>{
+    const allowedUpdates = ['description','completed']
+    const updateList = Object.keys(req.body)
+    const isallowed = updateList.every((update)=> allowedUpdates.includes(update))
+    if(!isallowed){
+        return res.status(400).send({error:`invalid update! only updates includes in the following list [${allowedUpdates}] is/are allowed to update` })
+    }
+
+    try{
+        const task = await Task.findOne({_id : req.params.id, owner: req.user._id})
+     
+       
+        if(!task){
+            return res.status(404).send()
+        }
+        updateList.forEach((update)=>task[update] = req.body[update])
+
+        await task.save()
+        res.send(task)
+
+    }catch(e){
+        console.log(e)
+        res.status(400).send()
+    }
+    
+
+})
+
+router.delete('/tasks/:id', auth , async (req, res)=>{
+    try{
+      const task = await Task.findOneAndDelete({_id:req.params.id, owner: req.user.id})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+
+    } catch(e){
+       
+        res.status(500).send()
+    }
+})
+
+module.exports = router
